43bd860a36b95f3199c7f0e1d429abe01608c556
4497cv <4497cv@gmail.com>
2019-09-26 14:55:49 -0500

    fixed some errors

--- HF_bandpass_filter.py
+++ HF_bandpass_filter.py
@@ -2,6 +2,10 @@ from butterworth import BP_BUTTERWORTH_FILTER
 from chebyshev_3db import BP_CHEBYSHEV_3DB_FILTER
 from chebyshev_0_5db import BP_CHEBYSHEV_0_5_DB_FILTER
 
+butterworth = 1
+chebyshev_0_5db = 2
+chebyshev_3db = 3
+
 def main():
     valid = 0
     
@@ -12,7 +16,7 @@ def main():
             print("2. Chebyshev 3.0 dB")
             select = int(input());
 
-            if(1 == select):
+            if(butterworth == select):
                     print("Introduce el orden 'N' del filtro (4,6,8):")
                     N = int(input());
                     print("Introduce el ancho de banda % 'BW %' del filtro: ")
@@ -23,7 +27,7 @@ def main():
                     R0 = float(input());
                     BP_BUTTERWORTH_FILTER(N, BW/100, fc, R0)
                     valid = 1
-            elif(2 == select):
+            elif(chebyshev_0_5db == select):
                     print("Introduce el orden 'N' del filtro (4,6,8):")
                     N = int(input());
                     print("Introduce el ancho de banda % 'BW %' del filtro: ")
@@ -32,9 +36,9 @@ def main():
                     fc = int(input());
                     print("Introduce la resistencia del sistema 'R0' del filtro: ")
                     R0 = float(input());
-                    BP_CHEBYSHEV_0_5DB_FILTER(N, BW/100, fc, R0)
+                    BP_CHEBYSHEV_0_5_DB_FILTER(N, BW/100, fc, R0)
                     valid = 1
-            elif(3 == select):
+            elif(chebyshev_3db == select):
                     print("Introduce el orden 'N' del filtro (4,6,8):")
                     N = int(input());
                     print("Introduce el ancho de banda % 'BW %' del filtro: ")
@@ -48,6 +52,7 @@ def main():
             else:
                     print("INDICE INVALIDO")
     
+    print("presione cualquier tecla para salir")
     input()
 
 main()
